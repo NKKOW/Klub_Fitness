aebd1fe58c38cc4653bfb7a202d68179814b5ee4
NKKOW <nina.kovalczuk@interia.eu>
2025-04-06 16:32:28 +0200

    database creation

---
+++ KlubFitness/src/main/java/org/example/klubfitness/KlubFitnessApplication.java
@@ -0,0 +1,13 @@
+package org.example.klubfitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlubFitnessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KlubFitnessApplication.class, args);
+    }
+
+}
